b00b4ffe9c160082584edb149606950c56c7a9cf
Pyongu <paulpkim845@gmail.com>
2025-05-02 22:21:40 -0700

    Added indexes

--- services/pg_normalized_batch/schema-indexes.sql
+++ services/pg_normalized_batch/schema-indexes.sql
@@ -2,6 +2,4 @@ CREATE INDEX on tweet_tags USING btree (id_tweets, tag);
 CREATE INDEX on tweet_tags USING btree (tag, id_tweets);
 CREATE INDEX on tweets USING btree (id_tweets, lang);
 CREATE INDEX on tweets USING btree (lang); 
-CREATE INDEX idx_tweets_text_lang
-ON tweets
-USING gin(to_tsvector('english', text));
+CREATE INDEX ON tweets USING gin(to_tsvector('english', text));
